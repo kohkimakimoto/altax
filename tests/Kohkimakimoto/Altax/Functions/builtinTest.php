30be4cfbd202af754fe3496314ea7d65e66a7881
kohkimakimoto <kohki.makimoto@gmail.com>
2013-09-26 14:25:52 +0900

    Add empty test

---
+++ tests/Kohkimakimoto/Altax/Functions/builtinTest.php
@@ -0,0 +1,11 @@
+<?php
+namespace Test\Kohkimakimoto\Altax\Functions;
+
+use Kohkimakimoto\Altax\Application\AltaxApplication;
+
+class BuiltinFunctionsTest extends \PHPUnit_Framework_TestCase
+{
+    public function testDefault()
+    {
+    }
+}
