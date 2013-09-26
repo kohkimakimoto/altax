ffc5af033acdf5aeef3c079788ae0c39e24f8799
kohkimakimoto <kohki.makimoto@gmail.com>
2013-09-26 14:34:28 +0900

    Add tests

--- tests/Kohkimakimoto/Altax/Command/BaseCommandTest/.altax/default.php
+++ tests/Kohkimakimoto/Altax/Command/BaseCommandTest/.altax/default.php
@@ -12,4 +12,15 @@
 // Host and role configurations.
 //
 role('web', '127.0.0.1');
+role('web', array('192.168.0.1', '192.168.0.2'));
+
+set("param1", "aaaaaaaaaa");
+get("param1");
+
+desc('This is a sample task.');
+task('sample',array('roles' => 'web'), function($host, $args){
+
+  run('echo Hellow World!');
+
+});
 
