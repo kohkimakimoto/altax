2fbcca2c938af967ec485997cd1e66b9a23279c0
kohkimakimoto <kohki.makimoto@gmail.com>
2013-10-04 15:57:48 +0900

    Add Executor test

---
+++ tests/Kohkimakimoto/Altax/Task/ExecutorTest.php
@@ -0,0 +1,31 @@
+<?php
+namespace Test\Kohkimakimoto\Altax\Task;
+
+use Kohkimakimoto\Altax\Task\Executor;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\ArgvInput;
+use Symfony\Component\Console\Output\ConsoleOutput;
+use Symfony\Component\Console\Input\InputDefinition;
+
+class ExecutorTest extends \PHPUnit_Framework_TestCase
+{
+    public function testDefault()
+    {
+        $executor = new Executor();
+        $InputDefinition = new InputDefinition();
+        $InputDefinition->addArgument(new InputArgument("args"));
+
+        $input = new ArgvInput(null, $InputDefinition);
+        $input->setArgument("args", array());
+        $output = new ConsoleOutput();
+
+        host("127.0.0.1", "default");
+        task('test_task',array('roles' => 'default'), function($host, $args){
+
+          message("test ok");
+
+        });
+
+        $executor->execute("test_task", $input, $output);
+    }
+}
