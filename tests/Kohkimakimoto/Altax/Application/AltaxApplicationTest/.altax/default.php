b96edf8083b3fdf401683843f26037e28e4b44eb
kohkimakimoto <kohki.makimoto@gmail.com>
2013-11-28 15:57:33 +0900

    Update tests

--- tests/Kohkimakimoto/Altax/Application/AltaxApplicationTest/.altax/default.php
+++ tests/Kohkimakimoto/Altax/Application/AltaxApplicationTest/.altax/default.php
@@ -2,6 +2,9 @@
 set("hoge", "foo");
 get("hoge");
 
+$configuration = new \Kohkimakimoto\Altax\Util\Configuration();
+$configuration->loadHosts(array(__DIR__."/hosts.php"));
+
 role('localhost', 'localhost');
 role('web', array('192.168.0.1', '192.168.0.2'));
 
