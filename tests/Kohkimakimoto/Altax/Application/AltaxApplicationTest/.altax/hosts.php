4706813af2625eab684f65464ad0f5dbfd94c52a
kohkimakimoto <kohki.makimoto@gmail.com>
2013-11-28 15:59:07 +0900

    Update tests

---
+++ tests/Kohkimakimoto/Altax/Application/AltaxApplicationTest/.altax/hosts.php
@@ -0,0 +1,6 @@
+<?php
+return array(
+    "srv1.foobar.com" => array("port" => 22, "roles" => array("foo","dev")),
+    "srv2.foobar.com" => array("port" => 22, "roles" => array("foo","prod")),
+    "srv3.foobar.com" => array("port" => 22, "roles" => array("foo","prod")),
+);
