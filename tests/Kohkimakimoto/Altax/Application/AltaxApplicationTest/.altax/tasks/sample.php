152f20ad5e7dac5a817050f551f9628877c10cf7
kohkimakimoto <kohki.makimoto@gmail.com>
2013-11-28 16:31:38 +0900

    Update tests

---
+++ tests/Kohkimakimoto/Altax/Application/AltaxApplicationTest/.altax/tasks/sample.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Sample "Hellow" Task Class
+ */
+class HellowTask extends \Kohkimakimoto\Altax\Task\BaseTask
+{
+    public function configure()
+    {
+        return array(
+            "name"        => "hellow",
+            "description" => "echo hellow",
+            "roles" => "web"
+            );
+    }
+
+    public function execute($host, $args)
+    {
+        $this->run("echo Hellow on the $host");
+    }
+}
