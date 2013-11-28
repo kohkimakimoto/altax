93a5b913c196014fcdc6ac1f063ef8ad6c6b3415
kohkimakimoto <kohki.makimoto@gmail.com>
2013-11-28 16:07:37 +0900

    Update tests

--- tests/Kohkimakimoto/Altax/Application/AltaxApplicationTest/.altax/home.php
+++ tests/Kohkimakimoto/Altax/Application/AltaxApplicationTest/.altax/home.php
@@ -1,7 +1,13 @@
 <?php
-desc('This is a sample task.');
-task('sample', array('roles' => 'localhost'), function($host, $args){
+task('sample0', array('roles' => 'localhost'), function($host, $args){
 
     run("echo Hello");
 
+});
+
+
+desc('This is a sample task.');
+task('sample', array('roles' => 'localhost'), function($host, $args){
+
+    run_task("sample0");
 });
