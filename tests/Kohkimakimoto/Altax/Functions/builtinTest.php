95ea22091a72f2b91f86ad3dd9dece6f81176cc9
John P. Bloch <john.bloch@10up.com>
2013-12-20 01:36:22 -0700

    Add tests for before() and after() functions

--- tests/Kohkimakimoto/Altax/Functions/builtinTest.php
+++ tests/Kohkimakimoto/Altax/Functions/builtinTest.php
@@ -26,4 +26,90 @@ class BuiltinFunctionsTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals("ccc", get("test_val2/aaa/bbb"));
     }
 
+    public function testBeforeException()
+    {
+        $context = Context::initialize();
+        try {
+            before('anything', 'task');
+            $this->fail('before() should throw an exception if the first task does not exist');
+        } catch (\Exception $e) {
+            // Test passed
+        }
+        $context->set('tasks', array('anything' => array('callback')));
+        try {
+            before('anything', 'random');
+            $this->fail('before() should throw an exception if the second task does not exist');
+        } catch (\Exception $e) {
+            // Test passed
+        }
+    }
+
+    public function testBefore()
+    {
+        $task1 = 'task' . rand(0, 9);
+        $task2 = 'task' . rand(10, 99);
+        $task3 = 'task' . rand(100, 999);
+        $tasks = array(
+            $task1 => array('foo'),
+            $task2 => array('bar'),
+            $task3 => array('baz'),
+        );
+        $context = Context::initialize();
+        $context->set('tasks', $tasks);
+        before($task1, $task2);
+        before($task1, $task3);
+        before($task2, $task1);
+        before($task2, $task3);
+        $before = $context->get('before');
+        $this->assertArrayHasKey($task1, $before);
+        $this->assertTrue(in_array($task2, $before[$task1]));
+        $this->assertTrue(in_array($task3, $before[$task1]));
+        $this->assertArrayHasKey($task2, $before);
+        $this->assertTrue(in_array($task1, $before[$task2]));
+        $this->assertTrue(in_array($task3, $before[$task2]));
+    }
+
+    public function testAfterException()
+    {
+        $context = Context::initialize();
+        try {
+            after('anything', 'task');
+            $this->fail('after() should throw an exception if the first task does not exist');
+        } catch (\Exception $e) {
+            // Test passed
+        }
+        $context->set('tasks', array('anything' => array('callback')));
+        try {
+            after('anything', 'random');
+            $this->fail('after() should throw an exception if the second task does not exist');
+        } catch (\Exception $e) {
+            // Test passed
+        }
+    }
+
+    public function testAfter()
+    {
+        $task1 = 'task' . rand(0, 9);
+        $task2 = 'task' . rand(10, 99);
+        $task3 = 'task' . rand(100, 999);
+        $tasks = array(
+            $task1 => array('foo'),
+            $task2 => array('bar'),
+            $task3 => array('baz'),
+        );
+        $context = Context::initialize();
+        $context->set('tasks', $tasks);
+        after($task1, $task2);
+        after($task1, $task3);
+        after($task2, $task1);
+        after($task2, $task3);
+        $after = $context->get('after');
+        $this->assertArrayHasKey($task1, $after);
+        $this->assertTrue(in_array($task2, $after[$task1]));
+        $this->assertTrue(in_array($task3, $after[$task1]));
+        $this->assertArrayHasKey($task2, $after);
+        $this->assertTrue(in_array($task1, $after[$task2]));
+        $this->assertTrue(in_array($task3, $after[$task2]));
+    }
+
 }
