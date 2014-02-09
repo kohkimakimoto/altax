0684579bf04e9e94a3104c12d6056bfc872781f7
kohkimakimoto <kohki.makimoto@gmail.com>
2014-02-09 16:41:01 +0900

    Add script handler class

---
+++ tests/Altax/Script/ScriptHandlerTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace Test\Altax\Script;
+
+use Altax\Script\ScriptHandler;
+
+class ScriptHandlerTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testRemoveAutoloadFiles()
+    {
+        ScriptHandler::removeAutoloadFiles();
+    }
+}
