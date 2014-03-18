97760031d668b5430df81b37e6235e8e9dd132bf
kohkimakimoto <kohki.makimoto@gmail.com>
2014-03-18 16:06:22 +0900

    Implement env command

--- src/Altax/Command/Builtin/EnvCommand.php
+++ src/Altax/Command/Builtin/EnvCommand.php
@@ -5,6 +5,7 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Altax\Module\Env\Facade\Env;
 
 /**
  * Env Command
@@ -21,11 +22,18 @@ class EnvCommand extends \Symfony\Component\Console\Command\Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $container = $this->getApplication()->getContainer();
         $table = $this->getHelperSet()->get('table');
-        
         $table->setHeaders(array('key', 'value'));
         
+        $vars = Env::getVars();
+        
+        foreach ($vars as $key => $value) {
+            $table->addRow(array(
+                $key,
+                $value
+            ));
+        }
+
         $table->render($output);
     }
 }
