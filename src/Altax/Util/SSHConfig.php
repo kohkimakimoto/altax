a553fd3373d403e8c76ebf78f2c5fd9dd91b5efa
kohkimakimoto <kohki.makimoto@gmail.com>
2014-03-20 18:14:06 +0900

    Update

--- src/Altax/Util/SSHConfig.php
+++ src/Altax/Util/SSHConfig.php
@@ -6,12 +6,44 @@ namespace Altax\Util;
  */
 class SSHConfig
 {
+
+    // Refering the following code.
+    // https://gist.github.com/geeksunny/3376694
+    // https://github.com/fitztrev/shuttle
+    
+    /**
+     * parse ssh config
+     * @param  [type] $contents [description]
+     * @return [type]           [description]
+     */
     public static function parse($contents)
     {
         $arr = array();
 
-        // TODO: implement.
-        
+        $servers = array();
+
+        $lines = explode("\n", $contents);
+        foreach ($lines as $line) {
+
+            if (preg_match(
+                "/^([ \t#])[ \t]*([^ \t=]+)[ \t=]+(.*)$/", 
+                $line,
+                $matches)) {
+
+                if ($matches && count($matches) != 4) {
+                    continue;
+                }
+
+                $isComment = ($matches[1] == "#");
+                $first = $matches[2];
+                $second = $matches[3];
+
+//                print_r($matches)."\n";
+
+            }
+            
+        }
+
         return $arr;
     }
 }
