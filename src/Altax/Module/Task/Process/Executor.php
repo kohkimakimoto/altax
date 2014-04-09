2f46289a1f8e7617496feb1d0251a4747fbbd056
kohkimakimoto <kohki.makimoto@gmail.com>
2014-04-09 18:35:15 +0900

    Fix bug using ssh-agent

--- src/Altax/Module/Task/Process/Executor.php
+++ src/Altax/Module/Task/Process/Executor.php
@@ -49,7 +49,7 @@ class Executor
             $hasPassphrase = false;
             $validatingKey = null;
             foreach ($nodes as $node) {
-                if ($node->isUsedWithPassphrase() && !$node->useAgent()) {
+                if (!$node->useAgent() && $node->isUsedWithPassphrase()) {
                     $hasPassphrase = true;
                     $validatingKey = $node->getKeyOrDefault();
                 }
