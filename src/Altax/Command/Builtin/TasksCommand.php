7e4df146dbb6e6c6b10182f73d540f891a5b4dcf
Damien Pitard <damien.pitard@gmail.com>
2014-04-11 15:01:26 +0200

    fixed a bug when a format exception is thrown in Task command

--- src/Altax/Command/Builtin/TasksCommand.php
+++ src/Altax/Command/Builtin/TasksCommand.php
@@ -25,8 +25,7 @@ class TasksCommand extends Command
             ->setDefinition(new InputDefinition(array(
                 new InputOption('format', null, InputOption::VALUE_REQUIRED, 'To output list in other formats', 'txt')
             )))
-            ->setDescription('Lists defined tasks')
-        ;
+            ->setDescription('Lists defined tasks');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
@@ -66,7 +65,7 @@ class TasksCommand extends Command
             }
             $output->writeln(json_encode($data));
         } else {
-            throw new \InvalidArgumentException(sprintf('Unsupported format "%s".', $options['format']));
+            throw new \InvalidArgumentException(sprintf('Unsupported format "%s".', $format));
         }
     }
 }
