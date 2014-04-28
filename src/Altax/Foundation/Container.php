5962f0d026289cccaa17870b76de4c2a931ec8f7
kohkimakimoto <kohki.makimoto@gmail.com>
2014-04-28 13:06:12 +0900

    Update version to v3.0.8

--- src/Altax/Foundation/Container.php
+++ src/Altax/Foundation/Container.php
@@ -17,7 +17,7 @@ class Container implements \ArrayAccess, \Iterator, \Countable
     /**
      * Version of the application.
      */
-    const VERSION = "3.0.7";
+    const VERSION = "3.0.8";
 
     /**
      * git commit hash.
