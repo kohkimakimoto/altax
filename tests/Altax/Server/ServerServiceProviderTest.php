bd1efaed7fe6d25836ab37a79b1829a0e7ca2a93
kohkimakimoto <kohki.makimoto@gmail.com>
2014-09-19 13:20:22 +0900

    Update tests

---
+++ tests/Altax/Server/ServerServiceProviderTest.php
@@ -0,0 +1,27 @@
+<?php
+namespace Test\Altax\Server;
+
+use Altax\Server\ServerServiceProvider;
+use Altax\Server\Server;
+use Altax\Server\KeyPassphraseMap;
+
+class ServerServiceProviderTest extends \PHPUnit_Framework_TestCase
+{
+    public function setup()
+    {
+        $this->app = bootAltaxApplication();
+    }
+
+    public function testMakeServer()
+    {
+        $server = $this->app["server"];
+        $this->assertTrue($server instanceof Server);
+    }
+
+    public function testMakeKeyPassphraseMap()
+    {
+        $server = $this->app["key_passphrase_map"];
+        $this->assertTrue($server instanceof KeyPassphraseMap);
+    }
+}
+
