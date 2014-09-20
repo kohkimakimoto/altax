91185f53120578d3d35ca2e7b940de7143728d97
kohkimakimoto <kohki.makimoto@gmail.com>
2014-09-20 10:33:14 +0900

    Rebuilding...

---
+++ src/Altax/Process/ProcessServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+namespace Altax\Process;
+
+use Illuminate\Support\ServiceProvider;
+
+class ProcessServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bindShared('process', function ($app) {
+            return new Process($app['server']);
+        });
+    }
+}
