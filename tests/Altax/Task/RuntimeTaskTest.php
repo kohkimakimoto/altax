d6658337194f85fcb04c5c7876a9eb6a12c5b945
kohkimakimoto <kohki.makimoto@gmail.com>
2014-09-20 11:37:55 +0900

    Rebuilding...

--- tests/Altax/Task/RuntimeTaskTest.php
+++ tests/Altax/Task/RuntimeTaskTest.php
@@ -19,9 +19,73 @@ class RuntimeTaskTest extends \PHPUnit_Framework_TestCase
     {
         $taskManager = $this->app["task"];
         $task = $taskManager->register("test", function(){});
-
         $runtimeTask = new RuntimeTask($task->makeCommand(), $task, $this->input, $this->output);
+
         $this->assertEquals("Altax\Task\RuntimeTask", get_class($runtimeTask));
+    }
+
+    public function testSetAndGetInput()
+    {
+        $taskManager = $this->app["task"];
+        $task = $taskManager->register("test", function(){});
+        $runtimeTask = new RuntimeTask($task->makeCommand(), $task, $this->input, $this->output);
+
+        $this->assertSame($this->input, $runtimeTask->getInput());
+        $input = new ArrayInput(array("command" => "test2"));
+        $runtimeTask->setInput($input);
+        $this->assertSame($input, $runtimeTask->getInput());
+        $this->assertNotSame($this->input, $runtimeTask->getInput());
+    }
+
+    public function testSetAndGetOutput()
+    {
+        $taskManager = $this->app["task"];
+        $task = $taskManager->register("test", function(){});
+        $runtimeTask = new RuntimeTask($task->makeCommand(), $task, $this->input, $this->output);
+
+        $this->assertSame($this->output, $runtimeTask->getOutput());
+        $output = new BufferedOutput();
+        $runtimeTask->setOutput($output);
+        $this->assertSame($output, $runtimeTask->getOutput());
+        $this->assertNotSame($this->output, $runtimeTask->getOutput());
+    }
+
+    public function testGetConfig()
+    {
+        $taskManager = $this->app["task"];
+        $task = $taskManager->register("test", function(){});
+        $runtimeTask = new RuntimeTask($task->makeCommand(), $task, $this->input, $this->output);
+
+        $this->assertSame($task->getConfig(), $runtimeTask->getConfig());
+    }
+
+    public function testWriteln()
+    {
+        $taskManager = $this->app["task"];
+        $task = $taskManager->register("test", function(){});
+        $runtimeTask = new RuntimeTask($task->makeCommand(), $task, $this->input, $this->output);
+
+        $runtimeTask->writeln("Write log test");
+        $this->assertEquals("Write log test\n", $this->output->fetch());
+    }
+
+    public function testWrite()
+    {
+        $taskManager = $this->app["task"];
+        $task = $taskManager->register("test", function(){});
+        $runtimeTask = new RuntimeTask($task->makeCommand(), $task, $this->input, $this->output);
+
+        $runtimeTask->write("Write log test");
+        $this->assertEquals("Write log test", $this->output->fetch());
+    }
+
+    public function testGetArguments()
+    {
+        $taskManager = $this->app["task"];
+        $task = $taskManager->register("test", function(){});
+        $runtimeTask = new RuntimeTask($task->makeCommand(), $task, $this->input, $this->output);
+
+        $args = $runtimeTask->getArguments();
     }
 }
 
---
+++ tests/Altax/Task/TaskTest.php
@@ -0,0 +1,46 @@
+<?php
+namespace Test\Altax\Task;
+
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\BufferedOutput;
+use Altax\Task\Task;
+
+class TaskTest extends \PHPUnit_Framework_TestCase
+{
+    public function setup()
+    {
+        $this->app = bootAltaxApplication();
+    }
+
+    public function testBasicAccessor()
+    {
+        $taskManager = $this->app["task"];
+        $task = $taskManager->register("test", function(){});
+
+        $task->setName("test2");
+        $this->assertEquals("test2", $task->getName());
+
+        $task->setDescription("dddd");
+        $this->assertEquals("dddd", $task->getDescription());
+        $this->assertEquals(true, $task->hasDescription());
+
+        $closure = function(){};
+        $task->setClosure($closure);
+        $this->assertEquals($closure, $task->getClosure());
+        $this->assertEquals(true, $task->hasClosure());
+
+        try {
+            $task->setClosure("aaaaa");
+            $this->assertEquals(true, false);
+        } catch (\RuntimeException $e) {
+            $this->assertEquals(true, true);
+        }
+
+        $task->setCommandClass("FooCommand");
+        $this->assertEquals("FooCommand", $task->getCommandClass());
+        $this->assertEquals(true, $task->hasCommandClass());
+
+        $task->setConfig(array("key" => "value"));
+        $this->assertSame(array("key" => "value"), $task->getConfig());
+    }
+}
