c73b9f95b73e825ae4135cba3d245858100a7e53
kohkimakimoto <kohki.makimoto@gmail.com>
2014-10-24 15:00:28 +0900

    Rebuilding...

--- src/Altax/Facade/Input.php
+++ src/Altax/Facade/Input.php
@@ -9,44 +9,4 @@ class Input extends \Illuminate\Support\Facades\Facade
      * @return string
      */
     protected static function getFacadeAccessor() { return 'input'; }
-
-    public function get($name)
-    {
-    }
-
-    public function argument($name)
-    {
-    }
-
-    public function option($name)
-    {
-    }
-
-    protected static function getDefaultArguments()
-    {
-        if (static::hasArgument('args')
-            && $args = static::getArgument('args')) {
-            return $args;
-        } else {
-            return null;
-        }
-    }
-
-    protected static function getDefaultArgument($index = 0, $default = null)
-    {
-        $retVal = null;
-        if ($args = static::getDefaultArguments()) {
-            if (isset($args[$index])) {
-                $retVal = $args[$index];
-            } else {
-                $retVal = $default;
-            }
-        } else {
-            $retVal = $default;
-        }
-
-        return $retVal;
-    }
-
-
 }
--- tests/Altax/Task/TaskManagerTest.php
+++ tests/Altax/Task/TaskManagerTest.php
@@ -1,11 +1,16 @@
 <?php
 namespace Test\Altax\Task;
 
+use Symfony\Component\Console\Output\BufferedOutput;
+use Symfony\Component\Console\Tester\CommandTester;
+use Altax\Console\Application;
+
 class TaskManagerTest extends \PHPUnit_Framework_TestCase
 {
     public function setup()
     {
         $this->app = bootAltaxApplication();
+        $this->app->instance("output", new BufferedOutput());
     }
 
     public function testRegisterClosure()
@@ -34,6 +39,37 @@ class TaskManagerTest extends \PHPUnit_Framework_TestCase
         $tasks = $taskManager->getTasks();
         $this->assertEquals(1, count($tasks));
     }
+
+    public function testCall()
+    {
+        $taskManager = $this->app["task"];
+        $task = $taskManager->register("test", function(){
+
+            $this->app["task"]->call("test2");
+
+        });
+        $task2 = $taskManager->register("test2", function(){
+
+            $this->app["output"]->write("called!");
+
+        });
+
+        $application = new Application($this->app);
+        $application->setAutoExit(false);
+        $application->add($task->makeCommand());
+        $application->add($$task2->makeCommand());
+        $command = $application->find("test");
+
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(
+            array(
+                "command" => $command->getName(),
+                )
+            );
+        $this->assertRegExp("/called!/", $this->app['output']->fetch());
+
+    }
+
 }
 
 class Test01Command extends \Altax\Command\Command
