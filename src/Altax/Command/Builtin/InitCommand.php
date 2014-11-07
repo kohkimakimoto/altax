680df64cb0a6f17f4d7e2a69e69bcea4a58d8cf3
kohkimakimoto <kohki.makimoto@gmail.com>
2014-11-07 16:08:52 +0900

    Update init command

--- src/Altax/Command/Builtin/InitCommand.php
+++ src/Altax/Command/Builtin/InitCommand.php
@@ -14,33 +14,13 @@ class InitCommand extends SymfonyCommand
 {
     const TEMPLATE = <<<EOL
 <?php
-// Autoloading for plugin command classes.
-if (is_file(__DIR__ . '/vendor/autoload.php')) require_once __DIR__ . '/vendor/autoload.php';
 
-// ***************************************************************
-// Server definition.
-// ***************************************************************
-//
-// Examples:
-//
-//   Server::node("web1.example.com", array("web", "production"));
-//   Server::node("web2.example.com", array("web", "production"));
-//   Server::node("db1.example.com",  array("db", "production"));
-//   Server::node("dev1.example.com", "development");
-//
+Task::register('hello', function (\$name = "world") {
 
-// ***************************************************************
-// Task definition.
-// ***************************************************************
-//
-// Examples:
-//
-//   Task::register('hello', function (\$task) {
-//
-//       \$task->writeln("Hello world!");
-//
-//   });
-//
+    Output::writeln("Hello \$name!");
+
+})
+->description("Say hello");
 
 EOL;
 
@@ -80,6 +60,7 @@ EOL;
             $configurationPath = getenv("HOME")."/.altax/config.php";
         }
 
+        $output->writeln("<info>Creating inital configuration.</info>");
         if (!is_file($configurationPath)) {
             $this->generateConfig($configurationPath, $output);
         } else {
