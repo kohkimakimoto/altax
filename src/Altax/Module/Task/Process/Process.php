0234aa90ac78b03cec5683d2cc43f7c80a9af8dc
Mark Kimsal <metrofindings@gmail.com>
2017-06-13 10:04:12 -0400

    Fix namespace again

--- src/Altax/Module/Task/Process/Process.php
+++ src/Altax/Module/Task/Process/Process.php
@@ -239,7 +239,7 @@ class Process
            throw new \RuntimeException("Couldn't put: $local -> $remote");
         }
 
-        $ret = $sftp->put($remote, $local, \NET\SFTP::SOURCE_LOCAL_FILE);
+        $ret = $sftp->put($remote, $local, \phpseclib\Net\SFTP::SOURCE_LOCAL_FILE);
         if ($ret === false) {
             throw new \RuntimeException("Couldn't put: $local -> $remote");
         }
@@ -266,7 +266,7 @@ class Process
 
     protected function getSFTP()
     {
-        $sftp = new \Net\SFTP(
+        $sftp = new \phpseclib\Net\SFTP(
             $this->node->getHostOrDefault(), 
             $this->node->getPortOrDefault());
         $key = new \phpseclib\Crypt\RSA();
